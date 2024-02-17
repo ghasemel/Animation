f2b2311ce7c35a05ad0131097761e47146285127
Ghasem <ghasem.elyasi@gmail.com>
2024-02-17 22:17:21 +0100

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Raouf on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
